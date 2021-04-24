c7760c61955b75151a2671c9d743ab20535bc324
Alexey <anborontov@gmail.com>
2021-04-24 18:13:50 +0300

    .

--- aioconsolemenu/templates.py
+++ aioconsolemenu/templates.py
@@ -3,10 +3,11 @@ import os
 
 from mako.lookup import TemplateLookup
 from loguru import logger
+import os
 
 lookup = TemplateLookup(
     [
-        "aioconsolemenu/templates/",
+        os.path.abspath("templates/"),
     ],
 )
 logger.debug(lookup.directories)
